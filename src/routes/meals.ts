46fd39d5a59a8992eb427800ad6432365d4afa6c
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2024-10-15 22:28:15 -0300

    fix config

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,3 +1,9 @@
-export function mealsRoutes () {
-  
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
+import { knex } from "../database";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
+
+export async function mealsRoutes () {
+
 }
