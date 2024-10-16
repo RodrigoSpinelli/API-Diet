6809e41704ef926759fbd1b9a7e160c92b87e575
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2024-10-15 22:22:32 -0300

    First commit configuration app

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import { app } from "./app";
+import { env } from "./env";
+
+const host = "RENDER" in process.env ? "0.0.0.0" : "localhost";
+
+app
+  .listen({ port: env.PORT, host })
+  .then(() => console.log(`HTTP Server Running on ${host}:${env.PORT}`));
